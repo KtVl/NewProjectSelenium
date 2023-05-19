fa06751ae7aea1820250c774bc1282aeaee31713
Kate <ktvlphoto@outlook.com>
2023-05-19 16:36:58 +0300

    добавила chromeOptions

--- src/main/java/com/geekbrains/lesson3/SeleniumStart.java
+++ src/main/java/com/geekbrains/lesson3/SeleniumStart.java
@@ -11,12 +11,16 @@ public class SeleniumStart {
 //
 //        WebDriver driver = new ChromeDriver();
 //        driver.get("https://xn------dddbhryrj2a0bnp2a3n.xn--p1ai/");
-        
-        WebDriver chromeDriver = new ChromeDriver();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications"); // открыть сайт без всплывающих окон
+        chromeOptions.addArguments("--headless");              // запустить не открывая браузер
+
+        WebDriver chromeDriver = new ChromeDriver(chromeOptions);
         chromeDriver.get("https://xn------dddbhryrj2a0bnp2a3n.xn--p1ai/");
 
-        chromeDriver.quit(); //закрыть браузер полностью
-//        chromeDriver.close(); // закрывает текущую вкладку
+        chromeDriver.quit();                                   //закрыть браузер полностью
+//        chromeDriver.close();                                // закрывает текущую вкладку
 
     }
 }
