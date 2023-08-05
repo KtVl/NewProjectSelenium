080bdbdadcd1878763765f3c493f8b7cd0d76340
Kate <ktvlphoto@outlook.com>
2023-08-05 12:07:54 +0300

    catEqualsAge Test

--- src/test/java/lesson4/FunctionsTest.java
+++ src/test/java/lesson4/FunctionsTest.java
@@ -1,10 +1,15 @@
 package lesson4;
 
+import com.geekbrains.lesson4.Cat;
 import com.geekbrains.lesson4.Functions;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static com.geekbrains.lesson4.Functions.isPalindrome;
 
@@ -60,6 +65,21 @@ public class FunctionsTest {
     void csvTest(String word, boolean expectedResult) {
         boolean actualResult = isPalindrome(word);
         Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @MethodSource("catAndAgeDataProvider")
+    void catEqualsAgeTest(Cat cat, Integer age) {
+        Assertions.assertEquals(cat.getAge(), age);
+    }
+
+    //метод источник тестовых данных для @MethodSource()
+    private static Stream<Arguments> catAndAgeDataProvider() {
+        return Stream.of(
+                Arguments.of(new Cat("test1", 10),10),
+                Arguments.of(new Cat("test2", 11),11),
+                Arguments.of(new Cat("test3", 15),15)
+        );
     }
 
     @AfterEach
